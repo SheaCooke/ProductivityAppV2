8a0bd5d6ccab5b77056d4fad64cb868d0aa6a9af
shea cooke <SheaSCooke@gmail.com>
2021-09-03 12:03:58 -0700

    started working on fetch and displaying stock info

--- ProductivityAppV2/Controllers/UserController.cs
+++ ProductivityAppV2/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace ProductivityAppV2.Controllers
 {
     public class UserController : Controller
@@ -131,6 +132,10 @@ namespace ProductivityAppV2.Controllers
 
             return View();
         }
+
+
+
+        
 
 
 
